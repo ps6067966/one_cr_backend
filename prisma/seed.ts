b66f522f87a192d38db561406dff32e3225ee976
Pratap Singh <ps6067966@gmail.com>
2023-03-05 22:39:26 +0530

    pss

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,28 +1,31 @@
-import {Prisma, PrismaClient} from '../generated/client/deno/edge.ts'
-import {config} from "https://deno.land/x/dotenv@v3.2.0/mod.ts";
+import { config } from "https://deno.land/x/dotenv@v3.2.0/mod.ts";
+import { Prisma, PrismaClient } from "../generated/client/deno/edge.ts";
 
 const envVars = config();
 
 const prisma = new PrismaClient({
-    datasources: {
-        db: {
-            url: envVars.DATABASE_URL,
-        }
+  datasources: {
+    db: {
+      url: envVars.DATABASE_URL,
     },
+  },
 });
 
 const usersData: Prisma.UserCreateInput[] = [
-    {
-        name: "Pratap",
-        email: "ps6067966@gmail.com"
-    },
+  {
+    name: "Pratap",
+    email: "ps6067966@gmail.com",
+  },
+  {
+    name: "Sachin",
+    email: "sachin123@gmail.com",
+  },
 ];
 
-for(const userData of usersData) {
-    await prisma.user.create({
-        data: userData,
-    });
+for (const userData of usersData) {
+  await prisma.user.create({
+    data: userData,
+  });
 }
 
 await prisma.$disconnect();
-
