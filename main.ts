6f2ada6618a54e1a1e0d85c42f903cd2c9863418
Pratap Singh <ps6067966@gmail.com>
2023-03-05 23:09:46 +0530

    pss

--- main.ts
+++ main.ts
@@ -1,8 +1,71 @@
-export function add(a: number, b: number): number {
-  return a + b;
-}
+import { config } from "https://deno.land/x/dotenv@v3.2.0/mod.ts";
+import { Application, Router } from "https://deno.land/x/oak@v12.0.0/mod.ts";
+import { PrismaClient } from "./generated/client/deno/edge.ts";
 
-// Learn more at https://deno.land/manual/examples/module_metadata#concepts
-if (import.meta.main) {
-  console.log("Add 2 + 3 =", add(2, 3));
-}
+const envVars = config();
+const prisma = new PrismaClient({
+  datasources: {
+    db: {
+      url: envVars.DATABASE_URL,
+    },
+  },
+});
+
+const app = new Application();
+const router = new Router();
+
+router.get("/", (ctx) => {
+  ctx.response.body = "Hello World!";
+})
+  .get("/users", async (ctx) => {
+    const users = await prisma.user.findMany();
+    ctx.response.body = users;
+  })
+  .get("/users/:id", async (ctx) => {
+    const user = await prisma.user.findUnique({
+      where: {
+        id: Number(ctx.params.id),
+      },
+    });
+    ctx.response.body = user;
+  })
+  .post("/users", async (ctx) => {
+    const { name, email } = await ctx.request.body().value;
+    const user = await prisma.user.create({
+      data: {
+        name,
+        email,
+      },
+    });
+    ctx.response.body = user;
+  })
+  .put("/users/:id", async (ctx) => {
+    const { name, email } = await ctx.request.body().value;
+    const user = await prisma.user.update({
+      where: {
+        id: Number(ctx.params.id),
+      },
+      data: {
+        name,
+        email,
+      },
+    });
+    ctx.response.body = user;
+  })
+  .delete("/users/:id", async (ctx) => {
+    const user = await prisma.user.delete({
+      where: {
+        id: Number(ctx.params.id),
+      },
+    });
+    ctx.response.body = user;
+  })
+  .delete("/users", async (ctx) => {
+    const users = await prisma.user.deleteMany();
+    ctx.response.body = users;
+  });
+
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+await app.listen({ port: 8000 });
