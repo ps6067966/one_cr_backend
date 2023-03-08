a949d876d117536a447a0f3d3d5e52ca5ac44aa1
Pratap Singh <ps6067966@gmail.com>
2023-03-08 19:24:19 +0530

    pss

---
+++ routes/bank_routes.ts
@@ -0,0 +1,67 @@
+import { Router } from "https://deno.land/x/oak@v12.0.0/mod.ts";
+import prisma from "./prisma_client.ts";
+
+const bankRouter = new Router();
+
+bankRouter
+  .get("/banks", async (ctx) => {
+    const banks = await prisma.bankAccountDetails.findMany();
+    ctx.response.body = banks;
+  })
+  .get("/banks/:email", async (ctx) => {
+    const bank = await prisma.bankAccountDetails.findUnique({
+      where: {
+        email: ctx.params.email,
+      },
+    });
+    ctx.response.body = bank;
+  })
+  .post("/banks", async (ctx) => {
+    const { email, full_name, paytm_number, upi_id, user_id, user } = await ctx
+      .request.body()
+      .value;
+    const bank = await prisma.bankAccountDetails.create({
+      data: {
+        full_name,
+        email,
+        paytm_number,
+        upi_id,
+        user_id,
+        user,
+      },
+    });
+    ctx.response.body = bank;
+  })
+  .put("/banks/:id", async (ctx) => {
+    const { email, full_name, paytm_number, upi_id, user_id, user } = await ctx
+      .request.body()
+      .value;
+    const bank = await prisma.bankAccountDetails.update({
+      where: {
+        id: Number(ctx.params.id),
+      },
+      data: {
+        email,
+        full_name,
+        paytm_number,
+        upi_id,
+        user,
+        user_id,
+      },
+    });
+    ctx.response.body = bank;
+  })
+  .delete("/banks/:id", async (ctx) => {
+    const bank = await prisma.bankAccountDetails.delete({
+      where: {
+        id: Number(ctx.params.id),
+      },
+    });
+    ctx.response.body = bank;
+  })
+  .delete("/banks", async (ctx) => {
+    const banks = await prisma.bankAccountDetails.deleteMany();
+    ctx.response.body = banks;
+  });
+
+export default bankRouter;
